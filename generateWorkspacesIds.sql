e0e421e18c57f86f39c7889a7bfc16fb710eb270
Trent Schafer <trent.schafer@gmail.com>
2016-01-09 16:07:58 +1100

    Add SQL script to output workspace IDs Looks like internal workspaces don't have the source_identifier set. Previously was using the condition:
    where workspace not in ('INTERNAL', 'COM.ORACLE.APEX.REPOSITORY');
    
    However, looking at apex 4 to 5, it looks like this list changes.

---
+++ generateWorkspacesIds.sql
@@ -0,0 +1,16 @@
+set echo off
+set verify off
+set feedback off
+set heading off
+set trimspool on
+set termout off
+set pagesize 0
+
+spool &1
+
+select to_char(workspace_id)
+from apex_workspaces
+where source_identifier is null;
+
+spool off
+exit
